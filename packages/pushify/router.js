0d4d1372c2b4953d5e4fa31e9ff8b933d4051dda
sketchytechky <boris.w.lau@gmail.com>
2016-01-23 23:14:58 -0800

    Added getChrome screen

--- packages/pushify/router.js
+++ packages/pushify/router.js
@@ -1,17 +1,36 @@
+/**
+ * Function for checking if we are using chrome
+ */
+function checkChrome(context, redirect) {
+    if (navigator.userAgent.indexOf('Chrome')===-1 && process.env.NODE_ENV==='production') {
+        redirect('/getchrome/');
+    }
+}
+
 FlowRouter.route("/", {
+    triggersEnter : [checkChrome],
     action: function() {
         ReactLayout.render(HomeLayout);
     }
 });
 
+FlowRouter.route("/getchrome/", {
+    action: function() {
+        ReactLayout.render(ChromeLayout);
+    }
+});
+
 FlowRouter.route("/subscribed/:slugname", {
     action: function() {
+        checkChrome();
         ReactLayout.render(SubscribedLayout);
     }
 });
 
 FlowRouter.route("/subscribe/:slugname/:tel", {
+    triggersEnter : [checkChrome],
     action: function() {
+        checkChrome();
         subscribePushNotification(
             FlowRouter.getParam('tel'),
             FlowRouter.getParam('slugname')
